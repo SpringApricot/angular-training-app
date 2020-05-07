6be7b4355c07067ee33472a0dfa69b0067a73c73
Joanna <azia54@gmail.com>
2020-05-07 03:55:53 +0200

    Login is functional.

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
     selector: 'app-nav',
@@ -10,7 +11,7 @@ import { Component } from '@angular/core';
 export class NavComponent {
     model: any = {};
 
-    constructor() {
+    constructor(private authService: AuthService) {
         //asd
     }
 
@@ -18,6 +19,22 @@ export class NavComponent {
     }
 
     login() {
-        console.log(this.model);
+        this.authService.login(this.model).subscribe(
+            next => {
+                console.log('Logged in succesfully!');
+            }, error => {
+                console.log("Failed to log in.");
+            }
+        );
+    }
+
+    loggedIn() {
+        const token = localStorage.getItem('token');
+        return !!token;
+    }
+
+    logout() {
+        localStorage.removeItem('token');
+        console.log('Logged out!');
     }
 }
